faf655b800eb998021bff00b8a18f5d3311f816c
Sagar Gupta <sg85207@gmail.com>
2023-05-04 13:40:51 +0530

    commit

--- SEM-4/KE Assignment/1st Assignment/3a_dataNormalization.cpp
+++ SEM-4/KE Assignment/1st Assignment/3a_dataNormalization.cpp
@@ -1,67 +1,73 @@
-// Q-3 Data Normalization 
+// Q-3 Data Normalization
 
 #include <bits/stdc++.h>
 using namespace std;
-void printValues(vector<double> a){
-int n=a.size();
-for(int i=0;i<n;i++)
-cout<<a[i]<<" ";
-cout<<endl;
+void printValues(vector<double> a)
+{
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
 }
-int main(){
-vector<double> a = {20,18,29,88,76,59,39,100,35,89,69,40,70};
-cout<<"Data Provided : ";
-printValues(a);
-int n=a.size();
-double maxi=a[0],mini=a[0];
-for(int i=0;i<n;i++){
-maxi=max(maxi,a[i]);
-mini=min(mini,a[i]);
-}
-double new_mini,new_maxi;
-cout<<"Enter new min : ";
-cin>>new_mini;
-cout<<"Enter new max : ";
-cin>>new_maxi;
-vector<double> b(n,0);
-for(int i=0;i<n;i++)
-b[i]=(a[i]-mini)*(new_maxi-new_mini)/double(maxi-mini);
-cout<<"Data in new Range : ";
-printValues(b);
+int main()
+{
+    vector<double> a = {20, 18, 29, 88, 76, 59, 39, 100, 35, 89, 69, 40, 70};
+    cout << "Data Provided : ";
+    printValues(a);
+    int n = a.size();
+    double maxi = a[0], mini = a[0];
+    for (int i = 0; i < n; i++)
+    {
+        maxi = max(maxi, a[i]);
+        mini = min(mini, a[i]);
+    }
+    double new_mini, new_maxi;
+    cout << "Enter new min : ";
+    cin >> new_mini;
+    cout << "Enter new max : ";
+    cin >> new_maxi;
+    vector<double> b(n, 0);
+    for (int i = 0; i < n; i++)
+        b[i] = (a[i] - mini) * (new_maxi - new_mini) / double(maxi - mini);
+    cout << "Data in new Range : ";
+    printValues(b);
 }
 
-
-//Z-SCORE///////////////////////////////////////////////////////
+// Z-SCORE///////////////////////////////////////////////////////
 
 #include <bits/stdc++.h>
 using namespace std;
-void printValues(vector<double> a){
-int n=a.size();
-for(int i=0;i<n;i++)
-cout<<a[i]<<" ";
-cout<<endl;
+void printValues(vector<double> a)
+{
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
 }
-double calculate_sd(vector<double> data) {
-double sum = 0.0, mean, standardDeviation = 0.0;
-for(int i = 0; i < 10; i++)
-sum += data[i];
-mean = sum / 10;
-for(int i = 0; i < 10; i++)
-standardDeviation += pow(data[i] - mean, 2);
-return sqrt(standardDeviation / 10);
+double calculate_sd(vector<double> data)
+{
+    double sum = 0.0, mean, standardDeviation = 0.0;
+    for (int i = 0; i < 10; i++)
+        sum += data[i];
+    mean = sum / 10;
+    for (int i = 0; i < 10; i++)
+        standardDeviation += pow(data[i] - mean, 2);
+    return sqrt(standardDeviation / 10);
 }
-int main(){
-vector<double> a = {20,18,29,88,76,59,39,100,35,89,69,40,70};
-double sum=0;
-int n=a.size();
-for(int i=0;i<n;i++)
-sum+=a[i];
-double mean = sum/n, sd;
-sd = calculate_sd(a);
-vector<double> b(n,0);
-for(int i=0;i<n;i++)
-b[i] = (a[i]-mean)/sd;
-cout<<"Data Provided : ";
-printValues(a);
-cout<<"Data after performing Z-score : ";
-printValues(b);}
+int main()
+{
+    vector<double> a = {20, 18, 29, 88, 76, 59, 39, 100, 35, 89, 69, 40, 70};
+    double sum = 0;
+    int n = a.size();
+    for (int i = 0; i < n; i++)
+        sum += a[i];
+    double mean = sum / n, sd;
+    sd = calculate_sd(a);
+    vector<double> b(n, 0);
+    for (int i = 0; i < n; i++)
+        b[i] = (a[i] - mean) / sd;
+    cout << "Data Provided : ";
+    printValues(a);
+    cout << "Data after performing Z-score : ";
+    printValues(b);
+}
--- SEM-4/KE Assignment/1st Assignment/4_Regression.cpp
+++ SEM-4/KE Assignment/1st Assignment/4_Regression.cpp
@@ -1,49 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-int sum(vector<int> a){
-int sum=0;
-for(int i: a)
-sum+=i;
-return sum;
+int sum(vector<int> a)
+{
+    int sum = 0;
+    for (int i : a)
+        sum += i;
+    return sum;
 }
-int sum_of_square(vector<int> a){
-int sum=0;
-for(int i: a)
-sum+=i*i;
-return sum;
+int sum_of_square(vector<int> a)
+{
+    int sum = 0;
+    for (int i : a)
+        sum += i * i;
+    return sum;
 }
-int sum_of_product(vector<int> a,vector<int> b){
-int n=a.size();
-int sum=0;
-for(int i=0;i<n;i++)
-sum+=a[i]*b[i];
-return sum;
+int sum_of_product(vector<int> a, vector<int> b)
+{
+    int n = a.size();
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += a[i] * b[i];
+    return sum;
 }
-void printcoords(vector<int> x,vector<int> y){
-int n=x.size();
-for(int i=0; i<n; i++){
-cout<<"("<<x[i]<<", "<<y[i]<<")";
-if(i!=n-1) cout<<",";
+void printcoords(vector<int> x, vector<int> y)
+{
+    int n = x.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << "(" << x[i] << ", " << y[i] << ")";
+        if (i != n - 1)
+            cout << ",";
+    }
 }
-}
-int main() {
-vector<int> x = {8,7,3,5,9,1,6,2,7,2};
-vector<int> y = {2,1,4,9,4,2,8,6,9,5};
-int n = x.size(), sum_of_x, sum_of_y, sq_sum_x, p_sum_xy;
-sum_of_x = sum(x);
-sum_of_y = sum(y);
-sq_sum_x = sum_of_square(x);
-p_sum_xy = sum_of_product(x,y);
-double a = double(sum_of_y*sq_sum_x-sum_of_x*p_sum_xy)/double(n*sq_sum_x-sum_of_x*sum_of_x);
-double b = double(n*p_sum_xy-sum_of_x*sum_of_y)/double(n*p_sum_xy-sum_of_x*sum_of_x);
-cout<<"Equation of line is : y = "<<a<<" + "<<b<<"x"<<endl;
-vector<int> ynew(n,0);
-for(int i=0;i<n;i++)
-ynew[i]=a+b*x[i];
-cout<<"Old Coords are : ";
-printcoords(x,y);
-cout<<endl;
-cout<<"New Coords are : ";
-printcoords(x,ynew);
-return 0;
+int main()
+{
+    vector<int> x = {8, 7, 3, 5, 9, 1, 6, 2, 7, 2};
+    vector<int> y = {2, 1, 4, 9, 4, 2, 8, 6, 9, 5};
+    int n = x.size(), sum_of_x, sum_of_y, sq_sum_x, p_sum_xy;
+    sum_of_x = sum(x);
+    sum_of_y = sum(y);
+    sq_sum_x = sum_of_square(x);
+    p_sum_xy = sum_of_product(x, y);
+    double a = double(sum_of_y * sq_sum_x - sum_of_x * p_sum_xy) / double(n * sq_sum_x - sum_of_x * sum_of_x);
+    double b = double(n * p_sum_xy - sum_of_x * sum_of_y) / double(n * p_sum_xy - sum_of_x * sum_of_x);
+    cout << "Equation of line is : y = " << a << " + " << b << "x" << endl;
+    vector<int> ynew(n, 0);
+    for (int i = 0; i < n; i++)
+        ynew[i] = a + b * x[i];
+    cout << "Old Coords are : ";
+    printcoords(x, y);
+    cout << endl;
+    cout << "New Coords are : ";
+    printcoords(x, ynew);
+    return 0;
 }
